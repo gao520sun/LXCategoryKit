63073655d2e1916f230559e79bbdfc4ea9005d2e
gzl <406243283@qq.com>
2019-12-23 22:49:25 +0800

    Initial commit

---
+++ Example/LXCategoryKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LXCategoryKit
+//
+//  Created by gao520sun on 12/23/2019.
+//  Copyright (c) 2019 gao520sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
